18b55520bce57279bb7cbf334779afb2b4cb0fc8
redpeacock78 <scarletpeacock78@gmail.com>
2019-08-05 11:07:29 +0900

    fix pceconv.rb

--- pceconv.rb
+++ pceconv.rb
@@ -3,8 +3,8 @@ require "formula"
 class Pceconv < Formula
   desc "It rips and converts data of PC engine (Super)CD-ROM^2."
   homepage "https://github.com/redpeacock78"
-  url "https://github.com/redpeacock78/pceconv"
-  head "https://github.com/redpeacock78/pceconv", :branch => "master"
+  url "https://github.com/redpeacock78/pceconv.git"
+  head "https://github.com/redpeacock78/pceconv.git", :branch => "master"
   version '0.0.1'.freeze
 
   #depends
