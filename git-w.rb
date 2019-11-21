f5cba8c3814cdb0806dca58112fb4654f815f218
redpeacock78 <scarletpeacock78@gmail.com>
2019-11-22 07:06:04 +0900

    Add content: git-w.rb

---
+++ git-w.rb
@@ -0,0 +1,22 @@
+require "formula"
+
+class Git_w < Formula
+  desc "It is a subcommand of the git command and displays github.com contribution."
+  homepage "https://github.com/redpeacock78"
+  url "https://github.com/redpeacock78/git-w.git"
+  head "https://github.com/redpeacock78/git-w.git", :branch => "master"
+  version '0.0.1'.freeze
+
+  #depends
+  depends_on "coreutils"
+  depends_on "findutils"
+  depends_on "bash"
+  depends_on "grep"
+  depends_on "git"
+
+  bottle :unneeded
+
+  def install
+    bin.install "git-w"
+  end
+end
