b8a52949c38cae192616101d1c18b573a866b193
redpeacock78 <scarletpeacock78@gmail.com>
2020-04-03 19:03:16 +0900

    Change content: mlterm-cocoa.rb

--- mlterm-cocoa.rb
+++ mlterm-cocoa.rb
@@ -5,9 +5,8 @@ class MltermCocoa < Formula
   url "https://downloads.sourceforge.net/project/mlterm/01release/mlterm-3.8.9/mlterm-3.8.9.tar.gz"
   head "https://bitbucket.org/arakiken/mlterm", :using => :hg
   sha256 "0d5f18c3986cc80cb8d0b1d8720a45be5eb85b2b908016f2f396bd5112e6c7c7"
-  keg_only "Conflicts with non-cocoa mlterm."
 
-  depends_on 'pkg-config' => :build
+  depends_on :x11
   depends_on 'gdk-pixbuf'
   depends_on 'cairo'
   depends_on 'sdl2'
@@ -22,8 +21,5 @@ class MltermCocoa < Formula
     system "./configure", "--with-gui=quartz", "--with-type-engines=cairo", "--with-imagelib=gdk-pixbuf", "--with-gtk=3.0", "--prefix=#{prefix}"
     system "make"
     system "make", "install"
-    system "cp -pvr cocoa/mlterm.app /Applications/"
-    system "mkdir -p /Applications/mlterm.app/Contents/MacOS"
-    system "HOME=/Applications cocoa/install.sh #{prefix}"
   end
 end
