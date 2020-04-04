0a605b6bcb1f0316261e93f16e7be768b431c3fd
redpeacock78 <scarletpeacock78@gmail.com>
2020-04-04 13:29:28 +0900

    Change content: tmux-sixel.rb

--- tmux-sixel.rb
+++ tmux-sixel.rb
@@ -26,16 +26,11 @@ class TmuxSixel < Formula
   depends_on "libevent"
   depends_on "ncurses"
   depends_on "utf8proc"
+  depends_on "libsixel"
 
   resource "completion" do
     url "https://raw.githubusercontent.com/imomaliev/tmux-bash-completion/homebrew_1.0.0/completions/tmux"
     sha256 "05e79fc1ecb27637dc9d6a52c315b8f207cf010cdcee9928805525076c9020ae"
-  end
-
-  def patches
-    [
-      "https://gist.githubusercontent.com/redpeacock78/42d7c3d5711a363492efe9d2da50a176/raw/6061b987d6afd3afcf319e6da44c50a1f3dd1a12/tmux-sixel.patch"
-    ]
   end
 
   def install
@@ -47,7 +42,15 @@ class TmuxSixel < Formula
       --prefix=#{prefix}
       --sysconfdir=#{etc}
     ]
+  end
 
+  def patches
+    [
+      "https://gist.githubusercontent.com/redpeacock78/42d7c3d5711a363492efe9d2da50a176/raw/6061b987d6afd3afcf319e6da44c50a1f3dd1a12/tmux-sixel.patch"
+    ]
+  end
+
+  def install
     ENV.append "LDFLAGS", "-lresolv"
     system "./configure", *args
 
