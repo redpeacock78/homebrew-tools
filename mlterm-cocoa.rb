084ca4bb0cb79b41e61ab7a16c637e6dc4bd16ad
redpeacock78 <scarletpeacock78@gmail.com>
2020-04-05 11:46:29 +0900

    Change content: mlterm-cocoa.rb

--- mlterm-cocoa.rb
+++ mlterm-cocoa.rb
@@ -2,12 +2,12 @@ require "formula"
 
 class MltermCocoa < Formula
   homepage "http://mlterm.sourceforge.net/"
-  url "https://downloads.sourceforge.net/project/mlterm/01release/mlterm-3.8.9/mlterm-3.8.9.tar.gz"
+  url "https://downloads.sourceforge.net/project/mlterm/01release/mlterm-3.6.0/mlterm-3.6.0.tar.gz"
   head "https://bitbucket.org/arakiken/mlterm", :using => :hg
-  sha256 "0d5f18c3986cc80cb8d0b1d8720a45be5eb85b2b908016f2f396bd5112e6c7c7"
-  version "3.8.9".freeze 
+  sha256 "b6b08e3ce3c2136ed091776f0bb3b9ad0b7070bc2d845c9debe6e2cdc34cec05"
+  version "3.6.0".freeze
 
-  depends_on :x11
+  depends_on 'pkg-config' => :build
   depends_on 'gdk-pixbuf'
   depends_on 'cairo'
   depends_on 'sdl2'
@@ -22,5 +22,8 @@ class MltermCocoa < Formula
     system "./configure", "--with-gui=quartz", "--with-type-engines=cairo", "--with-imagelib=gdk-pixbuf", "--with-gtk=3.0", "--prefix=#{prefix}"
     system "make"
     system "make", "install"
+    system "cp -pvr cocoa/mlterm.app /Applications/"
+    system "mkdir -p /Applications/mlterm.app/Contents/MacOS"
+    system "HOME=/Applications cocoa/install.sh #{prefix}"
   end
 end
