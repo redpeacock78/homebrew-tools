0f3e08160c1ec7b5cbffc59346c53029783d0254
redpeacock78 <scarletpeacock78@gmail.com>
2020-04-17 13:00:04 +0900

    Change content: libcpuid.rb

--- libcpuid.rb
+++ libcpuid.rb
@@ -1,10 +1,10 @@
-require "formula"
+require 'formula'
 
 class Libcpuid < Formula
-    desc "libcpuid is a small C library for x86 CPU detection and feature extraction."
-    homepage "http://libcpuid.sourceforge.net"
-    url "https://github.com/anrieff/libcpuid.git"
-    head "https://github.com/anrieff/libcpuid.git", :branch => "master"
+    desc 'libcpuid is a small C library for x86 CPU detection and feature extraction.'
+    homepage 'https://libcpuid.sourceforge.io'
+    url 'https://github.com/anrieff/libcpuid.git'
+    head 'https://github.com/anrieff/libcpuid.git', :branch => 'master'
     version '0.4.1'.freeze
 
     #depends
@@ -13,11 +13,11 @@ class Libcpuid < Formula
     depends_on 'libtool' => :build
 
     def install
-        system "type", "glibtoolize", "2&>1", "/dev/null", "&&", ":", "||", "alias", "glibtoolize='libtoolize'"
-        system "glibtoolize"
-        system "autoreconf", "--install"
-        system "./configure", "$(brew diy)"
-        system "make"
-        system "make", "install"
+        system 'type', 'glibtoolize', '2&>1', '/dev/null', '&&', 'alias', 'libtoolize="glibtoolize"', '||', ':'
+        system 'glibtoolize'
+        system 'autoreconf', '--install'
+        system './configure', '--prefix=#{prefix}'
+        system 'make'
+        system 'make', 'install'
     end
 end
