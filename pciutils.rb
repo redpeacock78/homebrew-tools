4670a7d0bd129397d91d6ef60c877330ab4c6a38
redpeacock78 <scarletpeacock78@gmail.com>
2020-04-17 19:48:31 +0900

    Add content: pciutils.rb

---
+++ pciutils.rb
@@ -0,0 +1,17 @@
+require "formula"
+
+class Pciutils < Formula
+    desc "The PCI Utilities."
+    homepage "https://mj.ucw.cz/sw/pciutils/"
+    url "https://github.com/pciutils/pciutils.git"
+    head "https://github.com/pciutils/pciutils.git", :branch => "master"
+    version "3.6.4".freeze
+
+    #depends
+    depends_on "pkg-config" => :build
+
+    def install
+        system "make", "PREFIX=#{prefix}"
+        system "make", "install", "install-lib"
+    end
+end
