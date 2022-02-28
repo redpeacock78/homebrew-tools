94dfd38932769cb2b7f82c2a882dd65c8c53d565
redpeacock78 <scarletpeacock78@gmail.com>
2022-03-01 01:07:15 +0900

    change: Update sayaka.rb

--- sayaka.rb
+++ sayaka.rb
@@ -3,8 +3,8 @@ require "formula"
 class Sayaka < Formula
   desc "It is a terminal-specific twitter client."
   homepage "https://github.com/isaki68k/sayaka"
-  url "https://github.com/isaki68k/sayaka.git"
-  head "https://github.com/isaki68k/sayaka.git", :branch => "3.4.3"
+  url "https://github.com/isaki68k/sayaka.git", :tag => "3.4.3"
+  head "https://github.com/isaki68k/sayaka.git", :head => "master"
   version "3.4.3".freeze
 
   depends_on "vala"
