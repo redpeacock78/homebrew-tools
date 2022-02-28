125790f515fa79f217635b6d00743940a0aed320
redpeacock78 <scarletpeacock78@gmail.com>
2022-03-01 01:00:47 +0900

    change: Update sayaka.rb

--- sayaka.rb
+++ sayaka.rb
@@ -4,19 +4,18 @@ class Sayaka < Formula
   desc "It is a terminal-specific twitter client."
   homepage "https://github.com/isaki68k/sayaka"
   url "https://github.com/isaki68k/sayaka.git"
-  head "https://github.com/isaki68k/sayaka.git", :branch => "master"
-  version "3.5.3".freeze
+  head "https://github.com/isaki68k/sayaka.git", :branch => "3.4.3"
+  version "3.4.3".freeze
 
-  depends_on "gcc"
-  depends_on "mbedtls"
+  depends_on "vala"
+  depends_on "glib"
+  depends_on "gdk-pixbuf"
   depends_on "libjpeg"
-  depends_on "libpng"
-  depends_on "bsdmake"
+  depends_on "make"
 
     def install
-    system "./configure"
-    system "bsdmake", "all"
-    bin.install "src/sayaka"
-    bin.install "src/sixelv"
+        system "./configure"
+        system "make"
+        bin.install "vala/sayaka"
   end
 end
