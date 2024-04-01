d1dde2f83419fea7b285a30ba55ef4ec65702c99
redpeacock78 <scarletpeacock78@gmail.com>
2024-04-01 12:19:47 +0900

    feat: Update sayaka version to 3.4.6

--- sayaka.rb
+++ sayaka.rb
@@ -1,21 +1,22 @@
 require "formula"
 
 class Sayaka < Formula
-  desc "It is a terminal-specific twitter client."
+  desc "It is a terminal-specific Misskey client."
   homepage "https://github.com/isaki68k/sayaka"
   url "https://github.com/isaki68k/sayaka.git", :tag => "3.7.5"
   head "https://github.com/isaki68k/sayaka.git", :head => "master"
   version "3.7.5".freeze
 
-  depends_on "vala"
-  depends_on "glib"
-  depends_on "gdk-pixbuf"
-  depends_on "libjpeg"
-  depends_on "make"
+  depends_on "bsdmake"
+  depends_on "gcc"
+  depends_on "pkg-config"
+  depends_on "wslay"
+  depends_on "webp"
+  depends_on "mbedtls"
 
     def install
         system "./configure"
-        system "make -DRELEASE sayaka"
+        system "bsdmake -DRELEASE sayaka"
         bin.install "vala/sayaka"
   end
 end
