046785a73535783b953baacdb366d1330e116f7b
redpeacock78 <scarletpeacock78@gmail.com>
2024-04-01 12:08:15 +0900

    feat: Update sayaka version to 3.7.5

--- sayaka.rb
+++ sayaka.rb
@@ -3,9 +3,9 @@ require "formula"
 class Sayaka < Formula
   desc "It is a terminal-specific twitter client."
   homepage "https://github.com/isaki68k/sayaka"
-  url "https://github.com/isaki68k/sayaka.git", :tag => "3.4.6"
+  url "https://github.com/isaki68k/sayaka.git", :tag => "3.7.5"
   head "https://github.com/isaki68k/sayaka.git", :head => "master"
-  version "3.4.6".freeze
+  version "3.7.5".freeze
 
   depends_on "vala"
   depends_on "glib"
